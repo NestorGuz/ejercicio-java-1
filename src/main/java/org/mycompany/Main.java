c2c73134cda84dba5ea7cf5d0a05dd1bfefc448c
NestorGuz <nestor.guz@outlook.com>
2023-05-25 10:24:58 -0500

    Base

---
+++ src/main/java/org/mycompany/Main.java
@@ -0,0 +1,12 @@
+package org.mycompany;
+
+public class Main {
+    public static void main(String[] args) {
+        int resultado = sumar(10,20,30);
+        System.out.println(resultado);
+    }
+
+    public static int sumar(int num1, int num2, int num3){
+        return num1+num2+num3;
+    }
+}
